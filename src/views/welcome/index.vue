0262819157ebf3b7c6e8939a0d63ec3184d7089e
Valter Barros <valterinside@gmail.com>
2022-02-21 12:50:40 -0300

    master initial commit
    Signed-off-by: Valter Barros <valterinside@gmail.com>

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ myName }}</p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        myName: 'valter'
+      } 
+    }
+  }
+</script>
+
+<style scoped></style>
